d8363ae76921c584622ee68ed8bc72bd6ffcc12e
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-10-29 23:53:49 +0900

    fix: retrieve chosen property and destination related properties

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -218,7 +218,7 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 PropertyList.defaultProps = {
 	initialInput: {
 		page: 1,
-		limit: 3,
+		limit: 9,
 		sort: 'createdAt',
 		direction: 'DESC',
 		search: {
